ccc575b42e779141353b5c615280f84eace963e4
arnoson <omasocke@hotmail.de>
2022-01-16 02:31:03 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,70 @@
+import paper from 'paper'
+import { perforatePath, toXY } from './utils'
+
+// @ts-ignore
+paper.setup(document.querySelector('canvas'))
+
+const markerRegExp = new RegExp(/^(midi|data)-(in|out|inout)/)
+
+interface Handle {
+  id: `${Handle['type']}-${Handle['index']}`
+  type: `${'midi' | 'data'}-${'in' | 'out' | 'inout'}`
+  index: number
+  angle: number
+  delta: { x: number; y: number }
+  point: { x: number; y: number }
+}
+
+const getHandles = (markers: paper.Path[], shape: paper.Path) => {
+  const indexes: Record<string, number> = { in: 0, out: 0, inout: 0 }
+
+  return markers.map((marker) => {
+    const match = marker.name.match(markerRegExp)
+    const [, signal, direction] = match!
+
+    const index = indexes[direction]++
+    const type = `${signal}-${direction}` as Handle['type']
+    const id = `${type}-${index}` as Handle['id']
+
+    const vector = marker.firstSegment.point.subtract(marker.lastSegment.point)
+    vector.angle -= 90
+    const angle = +vector.angle.toFixed(2)
+
+    const { point } = shape.getIntersections(marker)[0]
+    const delta = point.subtract(vector.normalize().multiply(14)).round()
+
+    return { id, type, index, angle, delta: toXY(delta), point: toXY(point) }
+  })
+}
+
+export const compileShape = (svg: string) => {
+  const parent = paper.project.importSVG(svg) as paper.Group
+  // Get rid of the background layer.
+  parent.firstChild.remove()
+
+  // @ts-ignore
+  const shape = parent.children['shape'] as paper.Path
+
+  const markers = parent.getItems((item: paper.Path) =>
+    item.name.match(markerRegExp)
+  ) as paper.Path[]
+
+  // Fit the view to the shape (without the markers).
+  markers.forEach((el) => (el.visible = false))
+  paper.view.viewSize = parent.bounds.size.round()
+  parent.position = paper.view.center
+
+  const outline = shape.clone()
+  outline.name = 'outline'
+  outline.strokeColor = new paper.Color('black')
+
+  const handles = getHandles(markers, shape)
+
+  const pointsOnOutline = handles.map((el) => el.point)
+  perforatePath(outline, pointsOnOutline)
+
+  markers.forEach((el) => el.remove())
+  svg = paper.project.exportSVG({ asString: true }) as string
+
+  return { handles, svg }
+}
---
+++ src/utils.ts
@@ -0,0 +1,41 @@
+export const toXY = (point: paper.Point) => ({ x: point.x, y: point.y })
+
+export const perforatePath = (
+  path: paper.Path,
+  points: { x: number; y: number }[],
+  holeWidth = 24
+) => {
+  const offsets = points.map((hole) => path.getOffsetOf(new paper.Point(hole)))
+  const sorted = offsets.sort((a, b) => a - b)
+
+  let dashOffset = 0
+  const dashArray = []
+
+  let lastOffset = 0
+  for (const offset of sorted) {
+    const holeStart = offset - holeWidth / 2
+    const holeEnd = offset + holeWidth / 2
+    dashArray.push(holeStart - lastOffset, holeWidth)
+    lastOffset = holeEnd
+  }
+
+  dashArray.push(path.length - lastOffset)
+
+  const [firstDash] = dashArray
+  if (firstDash < 0) {
+    dashOffset = firstDash * -1
+    dashArray[dashArray.length - 1] += firstDash
+    dashArray.push(firstDash * -1)
+    dashArray[0] = 0
+  }
+
+  const lastDash = dashArray[dashArray.length - 1]
+  if (lastDash < 0) {
+    dashOffset = lastDash
+    dashArray[0] += lastDash
+    dashArray[dashArray.length - 1] *= -1
+  }
+
+  path.dashArray = dashArray
+  path.dashOffset = dashOffset
+}
